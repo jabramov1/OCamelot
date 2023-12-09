55554988cb72b735eb8b5f5e318215457371a9a5
Joseph Abramov <joseph1abramov@gmail.com>
2023-12-09 02:43:41 -0500

    graph takes multiple mas

--- lib/Grapher.ml
+++ lib/Grapher.ml
@@ -2,14 +2,25 @@ open CsvReader
 open MovingAverage
 module Gp = Gnuplot
 
+type moving_avg_type =
+  | Simple
+  | Exponential
+  | Weighted
+  | Triangular
+  | VolumeAdjusted
+
 module type GrapherType = sig
-  val graph :
-    ?m_avg:((CsvReader.t -> int -> float option list) * int) list ->
-    CsvReader.t ->
-    unit
+  val graph : ?m_averages:(moving_avg_type * int) list -> CsvReader.t -> unit
+  (** [graph ?m_averages data] plots the given data along with specified moving
+      averages.
+      @param m_averages
+        is an optional parameter specifying a list of moving average types and
+        their respective periods. Defaults to 50, 100, and 200 day moving
+        averages.
+      @param data is the CSV data to be plotted. *)
 end
 
-module Grapher = struct
+module Grapher : GrapherType = struct
   let convert_data data =
     let convert_row row =
       let date = CsvReader.get_date row in
@@ -20,6 +31,15 @@ module Grapher = struct
       (date, (op, hi, lo, cl))
     in
     List.map convert_row data
+
+  let match_moving_avg (ma_type : moving_avg_type) :
+      CsvReader.t -> int -> float option list =
+    match ma_type with
+    | Simple -> MovingAverage.simple_moving_avg
+    | Exponential -> MovingAverage.exp_moving_avg
+    | Weighted -> MovingAverage.weighted_moving_avg
+    | Triangular -> MovingAverage.triangular_moving_avg
+    | VolumeAdjusted -> MovingAverage.vol_adj_moving_avg
 
   let rec tuple_lists l1 l2 =
     if List.length l1 > List.length l2 then
@@ -38,34 +58,36 @@ module Grapher = struct
   let filter_float_list (data : float option list) : float list =
     List.filter_map (fun x -> x) data
 
-  let graph ?(m_avg = [ (MovingAverage.simple_moving_avg, 5) ]) data =
+  let graph ?(m_averages = [ (Simple, 50); (Simple, 100); (Simple, 200) ]) data
+      =
     let plot_data = convert_data data in
     let dates = List.map fst plot_data in
-    Printf.printf "Number of Dates: %d\n" (List.length dates);
-    List.iter (fun d -> Printf.printf "Date: %f\n" d) dates;
 
-    let moving_avg_fun, period = List.hd m_avg in
-    let moving_avg_data = filter_float_list (moving_avg_fun data period) in
-    Printf.printf "Number of Moving Average Points: %d\n"
-      (List.length moving_avg_data);
-    List.iter
-      (fun ma -> Printf.printf "Moving Average: %f\n" ma)
-      moving_avg_data;
+    (* Calculate all moving averages provided in the m_averages list *)
+    let moving_avg_data_lists =
+      List.map
+        (fun (ma_type, ma_period) ->
+          let ma_fun = match_moving_avg ma_type in
+          filter_float_list (ma_fun data ma_period))
+        m_averages
+    in
+    let data_mas =
+      List.map (fun ma_data -> tuple_lists dates ma_data) moving_avg_data_lists
+    in
 
-    let data_ma = tuple_lists dates moving_avg_data in
-    Printf.printf "Number of Combined Points: %d\n" (List.length data_ma);
-    (* List.iter (fun (d, ma) -> Printf.printf "Combined Point: %f, %f\n" d ma)
-       data_ma; *)
-    if List.length data_ma = 0 then
-      Printf.printf "Error: No data points to plot.\n"
-    else
-      let line_s = Gp.Series.lines_datey ~color:`Magenta data_ma in
-      let candle_data = Gp.Series.candles_date_ohlc ~color:`Blue plot_data in
-      let gp = Gp.create () in
-
-      Gp.plot_many gp
-        ~range:(Gp.Date (List.hd dates, List.hd (List.rev dates)))
-        ~format:"%b %d'%y" [ candle_data; line_s ];
-
-      Unix.sleep 1000
+    (* ... plotting logic ... *)
+    let gp = Gp.create () in
+    let candle_data = Gp.Series.candles_date_ohlc ~color:`Blue plot_data in
+    let ma_series =
+      List.mapi
+        (fun i ma_data ->
+          Gp.Series.lines_datey
+            ~color:(List.nth [ `Magenta; `Green; `Red ] i)
+            ma_data)
+        data_mas
+    in
+    Gp.plot_many gp
+      ~range:(Gp.Date (List.hd dates, List.hd (List.rev dates)))
+      ~format:"%b %d'%y" (candle_data :: ma_series);
+    Unix.sleep 1000
 end
--- lib/Grapher.mli
+++ lib/Grapher.mli
@@ -1,10 +1,21 @@
 open CsvReader
 
+type moving_avg_type =
+  | Simple
+  | Exponential
+  | Weighted
+  | Triangular
+  | VolumeAdjusted
+
 module type GrapherType = sig
-  val graph :
-    ?m_avg:((CsvReader.t -> int -> float option list) * int) list ->
-    CsvReader.t ->
-    unit
+  val graph : ?m_averages:(moving_avg_type * int) list -> CsvReader.t -> unit
+  (** [graph ?m_averages data] plots the given data along with specified moving
+      averages.
+      @param m_averages
+        is an optional parameter specifying a list of moving average types and
+        their respective periods. Defaults to 50, 100, and 200 day moving
+        averages.
+      @param data is the CSV data to be plotted. *)
 end
 
 module Grapher : GrapherType
