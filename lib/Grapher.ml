9c992805adee954232eae4ee3ca8125b311cbf7b
Joseph Abramov <joseph1abramov@gmail.com>
2023-12-09 00:52:44 -0500

    Moving average on graph

--- lib/Grapher.ml
+++ lib/Grapher.ml
@@ -21,16 +21,51 @@ module Grapher = struct
     in
     List.map convert_row data
 
-  let graph ?(m_avg = [ (MovingAverage.simple_moving_avg, 0) ]) data =
-    print_endline (List.nth m_avg 0 |> snd |> string_of_int);
+  let rec tuple_lists l1 l2 =
+    if List.length l1 > List.length l2 then
+      match l1 with
+      | _ :: t -> tuple_lists t l2
+      | _ -> failwith "error"
+    else if List.length l1 < List.length l2 then
+      match l2 with
+      | _ :: t -> tuple_lists l1 t
+      | _ -> failwith "error"
+    else
+      match (l1, l2) with
+      | h1 :: t1, h2 :: t2 -> (h1, h2) :: tuple_lists t1 t2
+      | _ -> []
+
+  let filter_float_list (data : float option list) : float list =
+    List.filter_map (fun x -> x) data
+
+  let graph ?(m_avg = [ (MovingAverage.simple_moving_avg, 5) ]) data =
     let plot_data = convert_data data in
     let dates = List.map fst plot_data in
-    let start = List.hd dates in
-    let stop = List.hd (List.rev dates) in
-    let candle_data = Gp.Series.candles_date_ohlc plot_data in
-    let gp = Gp.create () in
-    Gp.plot gp
-      ~range:(Gp.Date (start -. (3600. *. 24.), stop +. (3600. *. 24.)))
-      ~format:"%b %d'%y" candle_data;
-    Unix.sleep 1000
+    Printf.printf "Number of Dates: %d\n" (List.length dates);
+    List.iter (fun d -> Printf.printf "Date: %f\n" d) dates;
+
+    let moving_avg_fun, period = List.hd m_avg in
+    let moving_avg_data = filter_float_list (moving_avg_fun data period) in
+    Printf.printf "Number of Moving Average Points: %d\n"
+      (List.length moving_avg_data);
+    List.iter
+      (fun ma -> Printf.printf "Moving Average: %f\n" ma)
+      moving_avg_data;
+
+    let data_ma = tuple_lists dates moving_avg_data in
+    Printf.printf "Number of Combined Points: %d\n" (List.length data_ma);
+    (* List.iter (fun (d, ma) -> Printf.printf "Combined Point: %f, %f\n" d ma)
+       data_ma; *)
+    if List.length data_ma = 0 then
+      Printf.printf "Error: No data points to plot.\n"
+    else
+      let line_s = Gp.Series.lines_datey ~color:`Magenta data_ma in
+      let candle_data = Gp.Series.candles_date_ohlc ~color:`Blue plot_data in
+      let gp = Gp.create () in
+
+      Gp.plot_many gp
+        ~range:(Gp.Date (List.hd dates, List.hd (List.rev dates)))
+        ~format:"%b %d'%y" [ candle_data; line_s ];
+
+      Unix.sleep 1000
 end
